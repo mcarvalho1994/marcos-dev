fd13aa8cd32340ba76e1d73b8373886f34d19187
Marcos Vinicios de Carvalho <marcos.vini.carvalho@hotmail.com>
2017-12-18 13:18:24 -0200

    segunda parte add section services

--- scripts/controllers/portfolioCtrl.js
+++ scripts/controllers/portfolioCtrl.js
@@ -6,6 +6,17 @@ angular.module("portfolio").controller("portfolioCtrl", function($scope, $http) 
 		{item: "Home", url: "#home"},
 		{item: "About me", url: "#about-me"},
 		{item: "Skills", url: "#skills"},
+		{item: "Services", url: "#services"},
 		{item: "Contact", url: "#contact"},
 	];
+
+	$scope.sItens =
+	[
+		{icon: "fa fa-desktop fa-2x", desc: "Configuração e formatação de computadores."},
+		{icon: "fa fa-server fa-2x", desc: "Gerenciamento de servidores."},
+		{icon: "fa fa-user  fa-2x", desc: "Suporte para usuários."},
+		{icon: "fa fa-shield fa-2x", desc: "Instalação de configuração do firewall pfSense."},
+		{icon: "fa fa-ticket fa-2x", desc: "Instalação de configuração do sistema de chamados GLPI."},
+		{icon: "fa fa-globe fa-2x", desc: "Desenvolvimento web."},
+	];	
 });
